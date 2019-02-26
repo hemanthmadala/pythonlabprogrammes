fd75d84cd1564364ab09c94fb6e574ad3b6ca717
hemanthmadala <47963843+hemanthmadala@users.noreply.github.com>
2019-02-26 12:41:55 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,4 @@
+import random
+l=['h',1,2.5,3,'s']
+l.append(66)
+print(l)
---
+++ swaptwovariables.py
@@ -0,0 +1,7 @@
+x=15
+y=30
+temp=x
+x=y
+y=temp
+print('The value of x is after swapping: {}'.format(x))
+print('The value of y is after swapping: {}'.format(y))
